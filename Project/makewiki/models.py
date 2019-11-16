b650817b28505555ebbeb09c30d8a3dbc26b739f
mohammed <mdrame113@gmail.com>
2019-11-16 11:55:15 -0800

    data sucessfully render from madel to static html

--- Project/makewiki/models.py
+++ Project/makewiki/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Database(models.Model):
+
+    title = models.CharField(max_length=100)
+    # date goes here 
+    authur = models.CharField(max_length=300)
+    body =  models.TextField()
+
+
+
+    def __stry__(self):
+        return self.title
--- Project/makewiki/views.py
+++ Project/makewiki/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Database
 
 # Create your views here.
 
 
 def homeView(request):
-    
-    return render(request, 'home.html')
+
+    context = {
+                'database': Database.objects.all()
+    }
+
+    return render(request, 'home.html', context)
